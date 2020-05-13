ca9aa5fe88023b4c9818407b28b754ed01f97edf
l326 <65176561+l326@users.noreply.github.com>
2020-05-13 15:17:26 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,7 @@
 package com.usian.java;
 
 public class Test1 {
-    //修改test1
+    //远程github仓库的代码
     public static void main(String[] args) {
         //github端（其他同事）的修改
         System.out.println("世界你好");
