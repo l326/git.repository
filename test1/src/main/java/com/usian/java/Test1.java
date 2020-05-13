d1a4f9df76964033c96089056b9574b2e3bb353d
l326 <3458262441@qq.com>
2020-05-13 15:16:26 +0800

    又修改Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,9 +1,9 @@
 package com.usian.java;
 
 public class Test1 {
-    //修改test1
+    //本地idea工作空间的代码
     public static void main(String[] args) {
         //github端（其他同事）的修改
-        System.out.prinln("世界你好");
+        System.out.println("世界你好");
     }
 }
