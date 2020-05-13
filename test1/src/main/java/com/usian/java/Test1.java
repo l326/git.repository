a03181727563a886020ca051792286fb431b9fc7
l326 <65176561+l326@users.noreply.github.com>
2020-05-13 15:06:41 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -3,6 +3,7 @@ package com.usian.java;
 public class Test1 {
     //修改test1
     public static void main(String[] args) {
-        
+        //github端（其他同事）的修改
+        System.out.prinln("世界你好");
     }
 }
